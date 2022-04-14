0742d33806b5f64c5a2c30b870e5d839ccfdf6ba
Luan Lara de Moraes <luanlaramoraes@gmail.com>
2022-04-14 09:42:11 -0300

    Aula

---
+++ src/espm/poo/aula05Exercicio2/PessoaJuridica.java
@@ -0,0 +1,15 @@
+package espm.poo.aula05Exercicio2;
+
+public class PessoaJuridica extends Cliente{
+    
+    private String cnpj;
+
+    public String getCnjp() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+}
---
+++ src/espm/poo/aula06/ClientePJ.java
@@ -0,0 +1,5 @@
+package espm.poo.aula06;
+
+public class ClientePJ {
+    
+}
